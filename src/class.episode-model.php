1a76f8b95b2732903bce3b4150914c07c943b429
Xavier Ivars-Ribes <xivarsribes@vistaprint.com>
2020-09-15 00:04:41 +0200

    Skeleton of EpisodeModel

---
+++ src/class.episode-model.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Class EpisodeModel
+ *
+ * Represents the model of a single episode
+ */
+class EpisodeModel
+{
+    private $_post;
+
+    public function __construct($post)
+    {
+        $this->_post = $post;
+    }
+
+    public function type() {
+        return $this->_post->episodetype;
+    }
+
+    public function number() {
+        return $this->_post->episode_number;
+    }
+
+    public function season() {
+        return $this->_post->episode_season;
+    }
+
+    public function title() {
+        return $this->_post->name;
+    }
+
+    public function description() {
+        return $this->_post->content;
+    }
+
+    public function image() {
+        var_dump($this->_post->image);
+    }
+
+    public function link() {
+        return $this->_post->link;
+    }
+
+    public function guid() {
+        return $this->_post->guid;
+    }
+}
